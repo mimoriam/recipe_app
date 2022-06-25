e655eaf9ffa8c5d1bb2877c2c789a70c493e30ef
Mimo <mimoriam420@gmail.com>
2022-06-25 18:48:53 +0500

    Commit #5 - Customized Django admin

--- core/models.py
+++ core/models.py
@@ -32,6 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
